054531629db13105e682af682bbf3e1e72412e37
LordeSid-04 <siddhanth.peshwani@gmail.com>
2025-04-20 03:49:52 +0800

    Update HDBStaff.java

--- 2002/src/HDBStaff.java
+++ 2002/src/HDBStaff.java
@@ -1,6 +1,9 @@
 import java.util.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.nio.charset.StandardCharsets;
 
 abstract class HDBStaff extends User {
     public HDBStaff(String name, String nric, int age, String maritalStatus, String password) {
