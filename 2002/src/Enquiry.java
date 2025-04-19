1dd2bbfe7921fdeda549e20f289911c8ce692cec
LordeSid-04 <siddhanth.peshwani@gmail.com>
2025-04-20 03:47:42 +0800

    Update Enquiry.java

--- 2002/src/Enquiry.java
+++ 2002/src/Enquiry.java
@@ -1,15 +1,18 @@
 import java.util.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.nio.charset.StandardCharsets;
 
 class Enquiry {
-    private Applicant applicant;
+    private User applicant; // Changed from Applicant to User
     private BTOProject project;
     private String message;
     private String reply;
     private LocalDate enquiryDate;
 
-    public Enquiry(Applicant applicant, BTOProject project, String message) {
+    public Enquiry(User applicant, BTOProject project, String message) {
         this.applicant = applicant;
         this.project = project;
         this.message = message;
@@ -19,7 +22,7 @@ class Enquiry {
     public String getMessage() { return message; }
     public String getReply() { return reply; }
     public void setReply(String reply) { this.reply = reply; }
-    public Applicant getApplicant() { return applicant; }
+    public User getApplicant() { return applicant; } // Changed return type to User
 
     @Override
     public String toString() {
